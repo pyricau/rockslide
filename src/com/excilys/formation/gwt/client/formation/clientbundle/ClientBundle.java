f5e922dba4e66e36277eda1910dc1a9987dedc02
matboniface <mat.boniface@gmail.com>
2011-04-27 01:18:16 +0200

    Ajout cours ClientBundle
    Wouuhhhhouuuu premier commit git !

--- src/com/excilys/formation/gwt/client/formation/clientbundle/ClientBundle.java
+++ src/com/excilys/formation/gwt/client/formation/clientbundle/ClientBundle.java
@@ -2,20 +2,34 @@ package com.excilys.formation.gwt.client.formation.clientbundle;
 
 import com.excilys.formation.gwt.client.formation.FormationGwt;
 import com.excilys.formation.gwt.client.slider.Chapter;
+import com.excilys.formation.gwt.client.slider.CodeSlide;
 import com.excilys.formation.gwt.client.slider.ElementSlide;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.client.ui.Widget;
 
 public class ClientBundle extends Chapter {
 
     @UiTemplate("Titre.ui.xml")
     interface Titre extends UiBinder<Element, ElementSlide> {}
 
+    @UiTemplate("Principe.ui.xml")
+    interface Principe extends UiBinder<Element, ElementSlide> {}
+
+    @UiTemplate("TextData.ui.xml")
+    interface TextData extends UiBinder<Widget, CodeSlide<?>> {}
+
+    @UiTemplate("I18N.ui.xml")
+    interface I18N extends UiBinder<Widget, CodeSlide<?>> {}
+
     @Override
     protected void buildSlides() {
         addSlide(GWT.create(Titre.class));
+        addSlide(GWT.create(Principe.class));
+        addCodeSlide(GWT.create(TextData.class));
+        addCodeSlide(GWT.create(I18N.class));
         addSlide(FormationGwt.PLAN);
     }
 
