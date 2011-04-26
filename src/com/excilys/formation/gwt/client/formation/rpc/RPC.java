7ba05924bb2be176f9feefe348fdd00f6b490487
pmerienne <pierre.merienne@excilys.com>
2011-04-26 16:04:27 +0200

    Ajout des slides du cours RPC

--- src/com/excilys/formation/gwt/client/formation/rpc/RPC.java
+++ src/com/excilys/formation/gwt/client/formation/rpc/RPC.java
@@ -13,9 +13,34 @@ public class RPC extends Chapter {
     @UiTemplate("Titre.ui.xml")
     interface Titre extends UiBinder<Element, ElementSlide> {}
 
+    @UiTemplate("CommunicationClassique.ui.xml")
+    interface CommunicationClassique extends UiBinder<Element, ElementSlide> {}
+
+    @UiTemplate("CommunicationGWT.ui.xml")
+    interface CommunicationGWT extends UiBinder<Element, ElementSlide> {}
+
+    @UiTemplate("CommunicationRPC.ui.xml")
+    interface CommunicationRPC extends UiBinder<Element, ElementSlide> {}
+
+    @UiTemplate("API.ui.xml")
+    interface API extends UiBinder<Element, ElementSlide> {}
+
+    @UiTemplate("Spring.ui.xml")
+    interface Spring extends UiBinder<Element, ElementSlide> {}
+
+    @UiTemplate("Autres.ui.xml")
+    interface Autres extends UiBinder<Element, ElementSlide> {}
+
     @Override
     protected void buildSlides() {
         addSlide(GWT.create(Titre.class));
+        addSlide(GWT.create(CommunicationClassique.class));
+        addSlide(GWT.create(CommunicationGWT.class));
+        addSlide(GWT.create(CommunicationRPC.class));
+        addSlide(GWT.create(API.class));
+        addPresentable(new AsyncCallback());
+        addSlide(GWT.create(Spring.class));
+        addSlide(GWT.create(Autres.class));
         addSlide(FormationGwt.PLAN);
     }
 
