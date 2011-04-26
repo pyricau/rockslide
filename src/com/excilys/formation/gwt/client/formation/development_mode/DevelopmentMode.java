74736d4e21d23ae8664edc7f2b592cc0c77604e8
pmerienne <pierre.merienne@excilys.com>
2011-04-26 11:50:07 +0200

    Ajout de la presentation Development Mode

---
+++ src/com/excilys/formation/gwt/client/formation/development_mode/DevelopmentMode.java
@@ -0,0 +1,26 @@
+package com.excilys.formation.gwt.client.formation.development_mode;
+
+import com.excilys.formation.gwt.client.formation.FormationGwt;
+import com.excilys.formation.gwt.client.slider.Chapter;
+import com.excilys.formation.gwt.client.slider.ElementSlide;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiTemplate;
+
+public class DevelopmentMode extends Chapter {
+
+    @UiTemplate("Titre.ui.xml")
+    interface Titre extends UiBinder<Element, ElementSlide> {}
+
+    @UiTemplate("Indispensable.ui.xml")
+    interface Indispensable extends UiBinder<Element, ElementSlide> {}
+
+    @Override
+    protected void buildSlides() {
+        addSlide(GWT.create(Titre.class));
+        addSlide(GWT.create(Indispensable.class));
+        addSlide(FormationGwt.PLAN);
+    }
+
+}
