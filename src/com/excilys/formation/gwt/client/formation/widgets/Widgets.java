cd4cf9a87dc563b83c8975c50a7a988db5875881
matboniface <mat.boniface@gmail.com>
2011-04-27 23:54:10 +0200

    Ajout d'un slide portant sur le composant Composite dans le cours de Widgets

--- src/com/excilys/formation/gwt/client/formation/widgets/Widgets.java
+++ src/com/excilys/formation/gwt/client/formation/widgets/Widgets.java
@@ -2,6 +2,7 @@ package com.excilys.formation.gwt.client.formation.widgets;
 
 import com.excilys.formation.gwt.client.formation.FormationGwt;
 import com.excilys.formation.gwt.client.slider.Chapter;
+import com.excilys.formation.gwt.client.slider.CodeSlide;
 import com.excilys.formation.gwt.client.slider.ElementSlide;
 import com.excilys.formation.gwt.client.slider.VisibleSlide;
 import com.google.gwt.core.client.GWT;
@@ -24,12 +25,16 @@ public class Widgets extends Chapter {
     @UiTemplate("Showcase.ui.xml")
     interface Showcase extends UiBinder<Widget, VisibleSlide> {}
 
+    @UiTemplate("Composite.ui.xml")
+    interface Composite extends UiBinder<Widget, CodeSlide<?>> {}
+
     @Override
     protected void buildSlides() {
         addSlide(GWT.create(Titre.class));
         addSlide(GWT.create(Presentation.class));
         addSlide(GWT.create(WidgetsGWT.class));
         addVisibleSlide(GWT.create(Showcase.class));
+        addCodeSlide(GWT.create(Composite.class));
         addSlide(FormationGwt.PLAN);
     }
 
