afae42402ea7b91d38f9f32d7d03e59aa3cbba8b
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-04-28 17:25:49 +0200

    added uibinder show notes

--- src/com/excilys/formation/gwt/client/formation/uibinder/UIBinder.java
+++ src/com/excilys/formation/gwt/client/formation/uibinder/UIBinder.java
@@ -1,9 +1,9 @@
 package com.excilys.formation.gwt.client.formation.uibinder;
 
 import com.excilys.formation.gwt.client.formation.FormationGwt;
+import com.excilys.formation.gwt.client.slider.shownotes.WidgetSlideNotes;
 import com.excilys.formation.gwt.client.slider.slides.Chapter;
 import com.excilys.formation.gwt.client.slider.slides.ElementSlide;
-import com.excilys.formation.gwt.client.slider.slides.WidgetSlide;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -16,7 +16,7 @@ public class UIBinder extends Chapter {
     interface Titre extends UiBinder<Element, ElementSlide> {}
 
     @UiTemplate("WithoutUiBinder.ui.xml")
-    interface WithoutUiBinder extends UiBinder<Widget, WidgetSlide> {}
+    interface WithoutUiBinder extends UiBinder<Widget, WidgetSlideNotes> {}
 
     @UiTemplate("Solution.ui.xml")
     interface Solution extends UiBinder<Element, ElementSlide> {}
@@ -25,23 +25,23 @@ public class UIBinder extends Chapter {
     interface UI extends UiBinder<Element, ElementSlide> {}
 
     @UiTemplate("HtmlCssJs.ui.xml")
-    interface HtmlCssJs extends UiBinder<Widget, WidgetSlide> {}
+    interface HtmlCssJs extends UiBinder<Widget, WidgetSlideNotes> {}
 
     @UiTemplate("XmlBinder.ui.xml")
-    interface XmlBinder extends UiBinder<Widget, WidgetSlide> {}
+    interface XmlBinder extends UiBinder<Widget, WidgetSlideNotes> {}
 
     @UiTemplate("JavaBinder.ui.xml")
-    interface JavaBinder extends UiBinder<Widget, WidgetSlide> {}
+    interface JavaBinder extends UiBinder<Widget, WidgetSlideNotes> {}
 
     @Override
     protected void buildSlides() {
         addSlide(GWT.create(Titre.class));
-        addWidgetSlide(GWT.create(WithoutUiBinder.class));
+        addWidgetNotesSlide(GWT.create(WithoutUiBinder.class));
         addSlide(GWT.create(Solution.class));
         addSlide(GWT.create(UI.class));
-        addWidgetSlide(GWT.create(HtmlCssJs.class));
-        addWidgetSlide(GWT.create(XmlBinder.class));
-        addWidgetSlide(GWT.create(JavaBinder.class));
+        addWidgetNotesSlide(GWT.create(HtmlCssJs.class));
+        addWidgetNotesSlide(GWT.create(XmlBinder.class));
+        addWidgetNotesSlide(GWT.create(JavaBinder.class));
         addSlide(FormationGwt.PLAN);
     }
 
