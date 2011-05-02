373e912fa874803b31eb0a001b812de3ba1bf84c
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-05-02 14:30:43 +0200

    show notes for widgets

--- src/com/excilys/formation/gwt/client/formation/widgets/Widgets.java
+++ src/com/excilys/formation/gwt/client/formation/widgets/Widgets.java
@@ -1,10 +1,10 @@
 package com.excilys.formation.gwt.client.formation.widgets;
 
 import com.excilys.formation.gwt.client.formation.FormationGwt.Plan;
+import com.excilys.formation.gwt.client.slider.shownotes.ElementSlideNotes;
+import com.excilys.formation.gwt.client.slider.shownotes.WidgetSlideNotes;
 import com.excilys.formation.gwt.client.slider.slides.Chapter;
-import com.excilys.formation.gwt.client.slider.slides.ElementSlide;
 import com.excilys.formation.gwt.client.slider.slides.VisibleSlide;
-import com.excilys.formation.gwt.client.slider.slides.WidgetSlide;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -14,17 +14,17 @@ import com.google.gwt.user.client.ui.Widget;
 public class Widgets extends Chapter {
 
     @UiTemplate("Titre.ui.xml")
-    interface Titre extends UiBinder<Element, ElementSlide> {
+    interface Titre extends UiBinder<Element, ElementSlideNotes> {
         Titre binder = GWT.create(Titre.class);
     }
 
     @UiTemplate("Presentation.ui.xml")
-    interface Presentation extends UiBinder<Element, ElementSlide> {
+    interface Presentation extends UiBinder<Element, ElementSlideNotes> {
         Presentation binder = GWT.create(Presentation.class);
     }
 
     @UiTemplate("WidgetsGWT.ui.xml")
-    interface WidgetsGWT extends UiBinder<Element, ElementSlide> {
+    interface WidgetsGWT extends UiBinder<Element, ElementSlideNotes> {
         WidgetsGWT binder = GWT.create(WidgetsGWT.class);
     }
 
@@ -34,17 +34,17 @@ public class Widgets extends Chapter {
     }
 
     @UiTemplate("Composite.ui.xml")
-    interface Composite extends UiBinder<Widget, WidgetSlide> {
+    interface Composite extends UiBinder<Widget, WidgetSlideNotes> {
         Composite binder = GWT.create(Composite.class);
     }
 
     @Override
     protected void buildSlides() {
-        addSlide(Titre.binder);
-        addSlide(Presentation.binder);
-        addSlide(WidgetsGWT.binder);
+        addNotesSlide(Titre.binder);
+        addNotesSlide(Presentation.binder);
+        addNotesSlide(WidgetsGWT.binder);
         addVisibleSlide(Showcase.binder);
-        addWidgetSlide(Composite.binder);
+        addWidgetNotesSlide(Composite.binder);
         addSlide(Plan.binder);
     }
 
