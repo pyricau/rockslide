fec5202ff1f1264c807f36d03cbd2fd888f4e117
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-05-02 14:19:51 +0200

    show notes for dev mode

--- src/com/excilys/formation/gwt/client/formation/development_mode/DevelopmentMode.java
+++ src/com/excilys/formation/gwt/client/formation/development_mode/DevelopmentMode.java
@@ -1,8 +1,8 @@
 package com.excilys.formation.gwt.client.formation.development_mode;
 
 import com.excilys.formation.gwt.client.formation.FormationGwt.Plan;
+import com.excilys.formation.gwt.client.slider.shownotes.ElementSlideNotes;
 import com.excilys.formation.gwt.client.slider.slides.Chapter;
-import com.excilys.formation.gwt.client.slider.slides.ElementSlide;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -11,19 +11,19 @@ import com.google.gwt.uibinder.client.UiTemplate;
 public class DevelopmentMode extends Chapter {
 
     @UiTemplate("Titre.ui.xml")
-    interface Titre extends UiBinder<Element, ElementSlide> {
+    interface Titre extends UiBinder<Element, ElementSlideNotes> {
         Titre binder = GWT.create(Titre.class);
     }
 
     @UiTemplate("Indispensable.ui.xml")
-    interface Indispensable extends UiBinder<Element, ElementSlide> {
+    interface Indispensable extends UiBinder<Element, ElementSlideNotes> {
         Indispensable binder = GWT.create(Indispensable.class);
     }
 
     @Override
     protected void buildSlides() {
-        addSlide(Titre.binder);
-        addSlide(Indispensable.binder);
+        addNotesSlide(Titre.binder);
+        addNotesSlide(Indispensable.binder);
         addSlide(Plan.binder);
     }
 
