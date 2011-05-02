a918426729286a44256b0d1141ebf92b27e1907e
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-05-02 13:48:26 +0200

    show notes for environnement_dev

--- src/com/excilys/formation/gwt/client/formation/environnement_dev/EnvironnementDev.java
+++ src/com/excilys/formation/gwt/client/formation/environnement_dev/EnvironnementDev.java
@@ -1,8 +1,8 @@
 package com.excilys.formation.gwt.client.formation.environnement_dev;
 
 import com.excilys.formation.gwt.client.formation.FormationGwt.Plan;
+import com.excilys.formation.gwt.client.slider.shownotes.ElementSlideNotes;
 import com.excilys.formation.gwt.client.slider.slides.Chapter;
-import com.excilys.formation.gwt.client.slider.slides.ElementSlide;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -11,25 +11,25 @@ import com.google.gwt.uibinder.client.UiTemplate;
 public class EnvironnementDev extends Chapter {
 
     @UiTemplate("Titre.ui.xml")
-    interface Titre extends UiBinder<Element, ElementSlide> {
+    interface Titre extends UiBinder<Element, ElementSlideNotes> {
         Titre binder = GWT.create(Titre.class);
     }
 
     @UiTemplate("GWTDesigner.ui.xml")
-    interface GWTDesigner extends UiBinder<Element, ElementSlide> {
+    interface GWTDesigner extends UiBinder<Element, ElementSlideNotes> {
         GWTDesigner binder = GWT.create(GWTDesigner.class);
     }
 
     @UiTemplate("SpeedTracer.ui.xml")
-    interface SpeedTracer extends UiBinder<Element, ElementSlide> {
+    interface SpeedTracer extends UiBinder<Element, ElementSlideNotes> {
         SpeedTracer binder = GWT.create(SpeedTracer.class);
     }
 
     @Override
     protected void buildSlides() {
-        addSlide(Titre.binder);
-        addSlide(GWTDesigner.binder);
-        addSlide(SpeedTracer.binder);
+        addNotesSlide(Titre.binder);
+        addNotesSlide(GWTDesigner.binder);
+        addNotesSlide(SpeedTracer.binder);
         addSlide(Plan.binder);
     }
 
