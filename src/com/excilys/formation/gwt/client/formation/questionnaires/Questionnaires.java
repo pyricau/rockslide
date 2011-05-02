2d535252759238ce502c87ed003464a05e3d81a5
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-05-02 15:43:06 +0200

    show notes for questionnaires

--- src/com/excilys/formation/gwt/client/formation/questionnaires/Questionnaires.java
+++ src/com/excilys/formation/gwt/client/formation/questionnaires/Questionnaires.java
@@ -1,7 +1,7 @@
 package com.excilys.formation.gwt.client.formation.questionnaires;
 
+import com.excilys.formation.gwt.client.slider.shownotes.ElementSlideNotes;
 import com.excilys.formation.gwt.client.slider.slides.Chapter;
-import com.excilys.formation.gwt.client.slider.slides.ElementSlide;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -10,13 +10,13 @@ import com.google.gwt.uibinder.client.UiTemplate;
 public class Questionnaires extends Chapter {
 
     @UiTemplate("Titre.ui.xml")
-    interface Titre extends UiBinder<Element, ElementSlide> {
+    interface Titre extends UiBinder<Element, ElementSlideNotes> {
         Titre binder = GWT.create(Titre.class);
     }
 
     @Override
     protected void buildSlides() {
-        addSlide(Titre.binder);
+        addNotesSlide(Titre.binder);
     }
 
 }
