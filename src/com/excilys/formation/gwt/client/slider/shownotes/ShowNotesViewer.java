e1f1beef9a7129472f8aeff503e6bd44e151fb92
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-05-02 15:00:46 +0200

    setting show notes window title

--- src/com/excilys/formation/gwt/client/slider/shownotes/ShowNotesViewer.java
+++ src/com/excilys/formation/gwt/client/slider/shownotes/ShowNotesViewer.java
@@ -3,12 +3,14 @@ package com.excilys.formation.gwt.client.slider.shownotes;
 import com.excilys.formation.gwt.client.slider.Resources;
 import com.excilys.formation.gwt.client.slider.window.WindowHelper;
 import com.excilys.formation.gwt.client.slider.window.WindowMessageListener;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class ShowNotesViewer {
 
     public void load() {
+        Window.setTitle("Show notes");
         Resources.instance.shownotes().ensureInjected();
         RootPanel rootPanel = RootPanel.get();
         final HTML html = new HTML();
