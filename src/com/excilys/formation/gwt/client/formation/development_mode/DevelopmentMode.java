e64df00511ce2c800f7e73f04d50088d80c380fc
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-05-03 23:37:37 +0200

    added a slide for debug showing

--- src/com/excilys/formation/gwt/client/formation/development_mode/DevelopmentMode.java
+++ src/com/excilys/formation/gwt/client/formation/development_mode/DevelopmentMode.java
@@ -2,11 +2,13 @@ package com.excilys.formation.gwt.client.formation.development_mode;
 
 import com.excilys.formation.gwt.client.formation.FormationGwt.Plan;
 import com.excilys.formation.gwt.client.slider.shownotes.ElementSlideNotes;
+import com.excilys.formation.gwt.client.slider.shownotes.WidgetSlideNotes;
 import com.excilys.formation.gwt.client.slider.slides.Chapter;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.client.ui.Widget;
 
 public class DevelopmentMode extends Chapter {
 
@@ -20,10 +22,16 @@ public class DevelopmentMode extends Chapter {
         Indispensable binder = GWT.create(Indispensable.class);
     }
 
+    @UiTemplate("Debug.ui.xml")
+    interface Debug extends UiBinder<Widget, WidgetSlideNotes> {
+        Debug binder = GWT.create(Debug.class);
+    }
+
     @Override
     protected void buildSlides() {
         addNotesSlide(Titre.binder);
         addNotesSlide(Indispensable.binder);
+        addWidgetNotesSlide(Debug.binder);
         addSlide(Plan.binder);
     }
 
