02a9b2b720691aff758ce0b9769da8e960bfae33
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-05-24 11:02:37 +0200

    adding WIP pages

--- src/info/piwai/cv/gwt/client/content/fossprojects/FOSSProjects.java
+++ src/info/piwai/cv/gwt/client/content/fossprojects/FOSSProjects.java
@@ -2,6 +2,7 @@ package info.piwai.cv.gwt.client.content.fossprojects;
 
 import info.piwai.cv.gwt.client.content.CV.Sections;
 import info.piwai.cv.gwt.client.content.HeaderChapter;
+import info.piwai.cv.gwt.client.content.WIP;
 import info.piwai.cv.gwt.client.slider.slides.ElementSlide;
 
 import com.google.gwt.core.client.GWT;
@@ -19,6 +20,13 @@ public class FOSSProjects extends HeaderChapter {
     @Override
     protected void buildSlides() {
         addSlide(Title.binder);
+        addPresentable(new WIP("AndroidAnnotations"), "AndroidAnnotations");
+        addPresentable(new WIP("GWT Landlisde"), "GWTLandlisde");
+        addPresentable(new WIP("FunkyJFunctional"), "FunkyJFunctional");
+        addPresentable(new WIP("Android"), "Android");
+        addPresentable(new WIP("GWT"), "GWT");
+        addPresentable(new WIP("C++"), "C++");
+        addPresentable(new WIP("PHP"), "PHP");
         addSlide(Sections.binder);
     }
 }
