dfdb9287ba25829708f2860f4ab2c8e2e072a091
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-05-24 10:51:58 +0200

    removing old files from previous slides

--- src/info/piwai/cv/gwt/client/content/old/events/codesample/CancelActionHandler.java
+++
@@ -1,7 +0,0 @@
-package info.piwai.cv.gwt.client.content.old.events.codesample;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface CancelActionHandler extends EventHandler {
-    void onCancelAction(CancelActionEvent event);
-}
