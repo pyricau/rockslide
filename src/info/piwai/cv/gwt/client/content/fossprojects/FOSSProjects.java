ac296e4122cf9e143e84b3ceea9f21fd02b4148e
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-07-13 12:13:08 +0200

    added content to skills and foss projects

--- src/info/piwai/cv/gwt/client/content/fossprojects/FOSSProjects.java
+++ src/info/piwai/cv/gwt/client/content/fossprojects/FOSSProjects.java
@@ -2,7 +2,6 @@ package info.piwai.cv.gwt.client.content.fossprojects;
 
 import info.piwai.cv.gwt.client.content.CV.Sections;
 import info.piwai.cv.gwt.client.content.HeaderChapter;
-import info.piwai.cv.gwt.client.content.WIP;
 import info.piwai.cv.gwt.client.slider.slides.ElementSlide;
 
 import com.google.gwt.core.client.GWT;
@@ -17,34 +16,57 @@ public class FOSSProjects extends HeaderChapter {
         Title binder = GWT.create(Title.class);
     }
 
+    @UiTemplate("AndroidAnnotations.ui.xml")
+    interface AndroidAnnotations extends UiBinder<Element, ElementSlide> {
+        AndroidAnnotations binder = GWT.create(AndroidAnnotations.class);
+    }
+
+    @UiTemplate("GWTLandslide.ui.xml")
+    interface GWTLandslide extends UiBinder<Element, ElementSlide> {
+        GWTLandslide binder = GWT.create(GWTLandslide.class);
+    }
+
+    @UiTemplate("FunkyJFunctional.ui.xml")
+    interface FunkyJFunctional extends UiBinder<Element, ElementSlide> {
+        FunkyJFunctional binder = GWT.create(FunkyJFunctional.class);
+    }
+
+    @UiTemplate("2H4U.ui.xml")
+    interface GAME_2H4U extends UiBinder<Element, ElementSlide> {
+        GAME_2H4U binder = GWT.create(GAME_2H4U.class);
+    }
+
+    @UiTemplate("Android.ui.xml")
+    interface Android extends UiBinder<Element, ElementSlide> {
+        Android binder = GWT.create(Android.class);
+    }
+
+    @UiTemplate("GWT.ui.xml")
+    interface FOSS_GWT extends UiBinder<Element, ElementSlide> {
+        FOSS_GWT binder = GWT.create(FOSS_GWT.class);
+    }
+
+    @UiTemplate("CPP.ui.xml")
+    interface CPP extends UiBinder<Element, ElementSlide> {
+        CPP binder = GWT.create(CPP.class);
+    }
+
+    @UiTemplate("PHP.ui.xml")
+    interface PHP extends UiBinder<Element, ElementSlide> {
+        PHP binder = GWT.create(PHP.class);
+    }
+
     @Override
     protected void buildSlides() {
         addSlide(Title.binder);
-
-        // Add a code sample
-        addPresentable(new WIP("AndroidAnnotations"), "AndroidAnnotations");
-
-        // Name will change.. code sample too
-        addPresentable(new WIP("GWT Landlisde"), "GWTLandlisde");
-
-        // Code sample too
-        addPresentable(new WIP("FunkyJFunctional"), "FunkyJFunctional");
-
-        // old one and forplay new implementation
-        addPresentable(new WIP("2H4U"), "2H4U");
-
-        // involved in maven android community, worked on RoboGuice, mavenized
-        // ACRA
-        addPresentable(new WIP("Android"), "Android");
-
-        // Projects from google code
-        addPresentable(new WIP("GWT"), "GWT");
-
-        // 2H4U, OpenGF
-        addPresentable(new WIP("C++"), "C++");
-
-        // Plagiasi, Bisouland
-        addPresentable(new WIP("PHP"), "PHP");
+        addSlide(AndroidAnnotations.binder);
+        addSlide(GWTLandslide.binder);
+        addSlide(FunkyJFunctional.binder);
+        addPresentable(new ElementSlide(GAME_2H4U.binder), "2H4U");
+        addSlide(Android.binder);
+        addPresentable(new ElementSlide(FOSS_GWT.binder), "GWT");
+        addPresentable(new ElementSlide(CPP.binder), "C++");
+        addSlide(PHP.binder);
         addSlide(Sections.binder);
     }
 }
--- src/info/piwai/cv/gwt/client/content/skills/Skills.java
+++ src/info/piwai/cv/gwt/client/content/skills/Skills.java
@@ -2,7 +2,6 @@ package info.piwai.cv.gwt.client.content.skills;
 
 import info.piwai.cv.gwt.client.content.CV.Sections;
 import info.piwai.cv.gwt.client.content.HeaderChapter;
-import info.piwai.cv.gwt.client.content.WIP;
 import info.piwai.cv.gwt.client.slider.slides.ElementSlide;
 
 import com.google.gwt.core.client.GWT;
@@ -17,48 +16,51 @@ public class Skills extends HeaderChapter {
         Title binder = GWT.create(Title.class);
     }
 
+    @UiTemplate("ComputerLanguages.ui.xml")
+    interface ComputerLanguages extends UiBinder<Element, ElementSlide> {
+        ComputerLanguages binder = GWT.create(ComputerLanguages.class);
+    }
+
+    @UiTemplate("DevTools.ui.xml")
+    interface DevTools extends UiBinder<Element, ElementSlide> {
+        DevTools binder = GWT.create(DevTools.class);
+    }
+
+    @UiTemplate("Frameworks.ui.xml")
+    interface Frameworks extends UiBinder<Element, ElementSlide> {
+        Frameworks binder = GWT.create(Frameworks.class);
+    }
+
+    @UiTemplate("OSServersDB.ui.xml")
+    interface OSServersDB extends UiBinder<Element, ElementSlide> {
+        OSServersDB binder = GWT.create(OSServersDB.class);
+    }
+
+    @UiTemplate("SpokenLanguages.ui.xml")
+    interface SpokenLanguages extends UiBinder<Element, ElementSlide> {
+        SpokenLanguages binder = GWT.create(SpokenLanguages.class);
+    }
+
+    @UiTemplate("Methods.ui.xml")
+    interface Methods extends UiBinder<Element, ElementSlide> {
+        Methods binder = GWT.create(Methods.class);
+    }
+
+    @UiTemplate("FunctionalSkills.ui.xml")
+    interface FunctionalSkills extends UiBinder<Element, ElementSlide> {
+        FunctionalSkills binder = GWT.create(FunctionalSkills.class);
+    }
+
     @Override
     protected void buildSlides() {
         addSlide(Title.binder);
-
-        // Java 5/6 (SCJP 91%), PHP 5, C, XML, XHTML, CSS, Javascript, C++,
-        // Ruby, ADA, Python, Prolog, DarkBasic Pro, LaTeX
-        addPresentable(new WIP("Computer Languages"), "ComputerLanguages");
-
-        /*
-         * Build : Maven, Hudson/Jenkins, Sonar
-         * 
-         * VCS : Git, SVN
-         * 
-         * Eclipse
-         * 
-         * Hacker tools : Firebug, Charles Proxy, nmap
-         */
-        addPresentable(new WIP("Dev Tools"), "DevTools");
-
-        // Android (list apps on market), Spring 3.0 (IoC/MVC/AOP), Hibernate,
-        // GWT 2.0, Guice,
-        // RoboGuice, JUnit 4, Mockito, APT, CodeModel, Gson, Jackson, JEE 5,
-        // JAX-RS, EJB3, ejb3unit, RRDTool, RRD4J, symfony (1.0 et 1.2), Ruby On
-        // Rails, SDL, script.aculo.us, Prototype, Struts
-        addPresentable(new WIP("Frameworks"), "Frameworks");
-
-        /*
-         * Servers : JBoss, WebSphere AS, Tomcat Web Servers,Apache OS : Linux->
-         * Android, Ubuntu, MacOS, Windows DB : MySQL, SQLite, PostgreSQL,
-         * Oracle
-         */
-        addPresentable(new WIP("OS / Servers / Databases"), "OS-Servers-DB");
-
-        // English, french, spanish, swedish
-        addPresentable(new WIP("Spoken Languages"), "SpokenLanguages");
-
-        // Scrum, GTD, TDD, ISO 9001v2000
-        addPresentable(new WIP("Methods"), "Methods");
-
-        // logistique / supervision / maintenance, finance
-        addPresentable(new WIP("Functional Skills"), "FunctionalSkills");
-
+        addSlide(ComputerLanguages.binder);
+        addSlide(DevTools.binder);
+        addSlide(Frameworks.binder);
+        addPresentable(new ElementSlide(OSServersDB.binder), "OS-Servers-DB");
+        addSlide(SpokenLanguages.binder);
+        addSlide(Methods.binder);
+        addSlide(FunctionalSkills.binder);
         addSlide(Sections.binder);
     }
 }
