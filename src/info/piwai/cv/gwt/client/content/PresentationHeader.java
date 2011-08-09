6bfe85fa2c62b264873c10a812dc136d32192ccc
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-08-09 14:03:22 +0200

    Added Google +1 button in header. Fixes #5

--- src/info/piwai/cv/gwt/client/content/PresentationHeader.java
+++ src/info/piwai/cv/gwt/client/content/PresentationHeader.java
@@ -1,7 +1,9 @@
 package info.piwai.cv.gwt.client.content;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.ScriptElement;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -12,7 +14,21 @@ public class PresentationHeader extends Widget {
     private static final Binder binder = GWT.create(Binder.class);
 
     public PresentationHeader() {
-        setElement(binder.createAndBindUi(this));
+        Element headerElement = binder.createAndBindUi(this);
+
+        String plusOneButton = "<g:plusone href=\"http://cv.piwai.info\"></g:plusone>";
+        String innerHTML = headerElement.getInnerHTML();
+        headerElement.setInnerHTML(plusOneButton + innerHTML);
+
+        setElement(headerElement);
+
+        Document doc = Document.get();
+        ScriptElement script = doc.createScriptElement();
+        script.setSrc("https://apis.google.com/js/plusone.js");
+        script.setType("text/javascript");
+        script.setLang("javascript");
+        doc.getBody().appendChild(script);
+
     }
 
 }
