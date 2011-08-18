78077a06417c153bdaf4b61b9a524d718328f1bf
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-08-18 18:15:59 +0200

    added xml brush, changed color set

---
+++ rockslide-parent/rockslide/src/main/java/info/piwai/rockslide/sh/xml/client/XmlSH.java
@@ -0,0 +1,13 @@
+package info.piwai.rockslide.sh.xml.client;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+public class XmlSH {
+    
+    public native JavaScriptObject brush() /*-{
+        return new $wnd.SyntaxHighlighter.brushes.Xml();
+    }-*/;
+    
+    
+
+}
